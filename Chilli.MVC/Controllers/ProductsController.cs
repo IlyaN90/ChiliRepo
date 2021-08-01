ff305899c44485b5fd896c2cd218b813ea90423d
Ilya Nagibin <ilja.nagibin@entrydraft.se>
2021-08-01 23:49:35 +0200

    adds new Create, Details, Index for Product in MVC

---
+++ Chilli.MVC/Controllers/ProductsController.cs
@@ -0,0 +1,106 @@
+﻿using AutoMapper;
+using Chilli.API.Queries;
+using Chilli.Application.MediatR.Queries;
+using Chilli.Core.Infrastructure.Entities.Product;
+using Chilli.Core.Product.Models;
+using Chilli.Core.Product.Models.DBO;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chilli.MVC.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public ProductsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        // GET: ProductsController
+        public async Task<IActionResult> Index()
+        {
+            var query = new GetAllProductsQuery();
+            var result = await _mediator.Send(query);
+            return View(result.Products);
+        }
+
+        // GET: ProductsController/Details/5
+        public async Task<ActionResult> Details(int id)
+        {
+            var query = new GetProductByIdQuery(id);
+            var result = await _mediator.Send(query);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return View(result.Products.FirstOrDefault());
+        }
+
+        // GET: ProductsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
